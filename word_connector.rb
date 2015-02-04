ebed7ebc9f95a94a2b589694088209ec6a70eb6f
Luke Korth <lukekorth@gmail.com>
2015-02-03 18:36:15 -0600

    looking forward to pushing this because i am real confused as to why it wasn't working for a little while. working now anyway.

--- word_connector.rb
+++ word_connector.rb
@@ -2,10 +2,15 @@ require "pry"
 class WordConnector
   
   def self.word_connector(array_to_join)
-    last_word = array_to_join[-1]
-    array_to_join.delete_at(-1)
-    new_string = array_to_join.join(", ")
-    puts "#{new_string} and #{last_word}"
+    if array_to_join.count > 2
+      last_word = array_to_join[-1]
+      array_to_join.delete_at(-1)
+      new_string = array_to_join.join(", ")
+      puts "#{new_string}, and #{last_word}"
+    else
+      two_string = array_to_join.join(" and ")
+      puts "#{two_string}"
+    end
   end
     
   
